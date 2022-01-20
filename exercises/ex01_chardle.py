ca49508e67b5f3cba8f2b6384699a79c0efa8802
730477179 <aklilue@ad.unc.edu>
2022-01-20 18:48:52 -0500

    Chardle

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,7 @@
+"""EX01- Chardle - A cute step toward Wordle."""
+
+__author__= "730477179"
+
+asking_for_a_word: str = input("Enter a 5 character word")
+asking_for_a_single_character: str =input("Enter a single character")
+print: asking_for_a_word[0]+ asking_for_a_word[2]+asking_for_a_single_character[0]
