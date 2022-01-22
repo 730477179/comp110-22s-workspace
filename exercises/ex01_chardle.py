13aebf37bad19d64c3114c664e307cca52f2d1bf
730477179 <aklilue@ad.unc.edu>
2022-01-21 23:00:38 -0500

    Chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,6 +2,22 @@
 
 __author__= "730477179"
 
-asking_for_a_word: str = input("Enter a 5 character word")
-asking_for_a_single_character: str =input("Enter a single character")
-print: asking_for_a_word[0]+ asking_for_a_word[2]+asking_for_a_single_character[0]
+five_letter_word: str = input("Enter a 5 character word:")
+one_letter_input: str = input("Enter a single character:")
+matching_instance: one_letter_input
+print("searching for " + one_letter_input   +    " in "  +  five_letter_word )
+
+if (five_letter_word[0] == one_letter_input):
+    print(one_letter_input  + " found at index 0")
+    i
+if (five_letter_word[1] == one_letter_input):
+    print(one_letter_input + "found at index 1")
+if(five_letter_word[2] == one_letter_input):
+            print(one_letter_input + "found at index 2")
+if(five_letter_word[3] == one_letter_input):
+                print(one_letter_input + "found at index 3")
+if(five_letter_word[4] == one_letter_input):
+                    print(one_letter_input + "found at index 4")
+        
+
+
