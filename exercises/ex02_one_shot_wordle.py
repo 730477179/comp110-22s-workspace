d8dafadb9da690062996f3e6d82756a5e8512cb6
730477179 <aklilue@ad.unc.edu>
2022-01-30 12:23:18 -0500

    wordle better

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -5,18 +5,20 @@ __author__ = "730477179"
 SECRET: str = "python"
 six_letter_guess: str = input("What is your 6-letter guess?")
 
+while len(six_letter_guess) != len(SECRET):
+    new_input: str = input("That was not 6 letters! Try again: ") 
+
 if six_letter_guess == SECRET:
-    print ("Woo!You got it!")
+    print ( "Woo!You got it!")
 else:
     print("Not quite. Play again soon!")
 
-while len(six_letter_guess) != len(SECRET):
-    new_input: str = input("That was not 6 letters! Try again:") 
+
 
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
-YELLOW_BOX:str = "\U0001F7E8"
+YELLOW_BOX: str = "\U0001F7E8"
 
 i: int = 0
 emoji_storage: str = ""
@@ -32,16 +34,12 @@ while Guessing and idx < len(six_letter_guess):
     else: 
         emoji_storage = emoji_storage + YELLOW_BOX
 
-
-
 while i < len(SECRET):
     if six_letter_guess[i] == SECRET[i]:
         i= i + 1
-        emoji_storage = emoji_storage + GREEN_BOX
-        
+        emoji_storage = emoji_storage + GREEN_BOX       
     else: 
         emoji_storage = emoji_storage + WHITE_BOX
         i = i + 1
                
 print(emoji_storage)
-
