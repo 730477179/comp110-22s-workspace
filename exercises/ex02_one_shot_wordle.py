461a203ef1537c9fe5647a2d440412e47b62c6e9
730477179 <aklilue@ad.unc.edu>
2022-01-30 12:01:28 -0500

    better wordle

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,9 +1,6 @@
+"""One Shot Wordle."""
 
-"""One Shot Wordle"""
-__author__ = 730477179
-
-from cgi import test
-
+__author__ = "730477179"
 
 SECRET: str = "python"
 six_letter_guess: str = input("What is your 6-letter guess?")
@@ -14,7 +11,7 @@ else:
     print("Not quite. Play again soon!")
 
 while len(six_letter_guess) != len(SECRET):
-    new_input: str = input( "That was not 6 letters! Try again:") 
+    new_input: str = input("That was not 6 letters! Try again:") 
 
 
 WHITE_BOX: str = "\U00002B1C"
@@ -23,24 +20,28 @@ YELLOW_BOX:str = "\U0001F7E8"
 
 i: int = 0
 emoji_storage: str = ""
+Guessing: bool = False
+idx: int = 0
+
+while Guessing and idx < len(six_letter_guess): 
+    if SECRET[idx] == six_letter_guess[i]:
+        Guessing = True
+        emoji_storage = emoji_storage + GREEN_BOX
+        idx = idx + 1
+        
+    else: 
+        emoji_storage = emoji_storage + YELLOW_BOX
+
+
 
 while i < len(SECRET):
     if six_letter_guess[i] == SECRET[i]:
-        print(GREEN_BOX)
-        i= i+ 1
+        i= i + 1
+        emoji_storage = emoji_storage + GREEN_BOX
+        
     else: 
-        print(WHITE_BOX)
-        i = i+ 1
-    
-Guessing: bool = False
-idx: int = 0
-Guessing == False
-while Guessing and idx < len(SECRET): 
-    if SECRET[i] != six_letter_guess[idx]:
-        print(YELLOW_BOX)
-    else:
-        idx = idx + 1
-
-
-
+        emoji_storage = emoji_storage + WHITE_BOX
+        i = i + 1
+               
+print(emoji_storage)
 
