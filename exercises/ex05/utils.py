c6a0412860468c37d169c095c37761e74637b216
730477179 <aklilue@ad.unc.edu>
2022-02-20 21:25:53 -0500

    finished

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,35 @@
+"""EX05: Function and Test Practice."""
+
+
+def only_evens(a: list[int]) -> list[int]: 
+    """Find the even values in a list."""
+    i: int = 0
+    even_values: list[int] = list()
+    while i < len(a):
+        if a[i] % 2 == 0:
+            even_values.append(a[i])
+            i = i + 1
+        else:
+            i = i + 1
+    print(even_values)
+    return even_values
+
+
+def sub(a: list[int], b: int, c: int) -> list[int]:
+    """Give the values of list at range of index."""
+    new_list_for_sub: list[int] = []
+    i: int = b
+    if len(a) == 0 or b > len(a) or c <= 0:
+        return new_list_for_sub
+    elif b < 0:
+        b = a[0]
+        while i >= b and c <= len(a): 
+            new_list_for_sub.append(a[i])
+            i = i + 1
+    return new_list_for_sub
+
+
+def concat(a: list[int], b: list[int]) -> list[int]:
+    """Combines two lists of integers."""
+    all_values: list[int] = a + b
+    return all_values
