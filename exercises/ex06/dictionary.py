47ffdff9b66cc9c7d3130870e9eb22d66a6925e6
730477179 <aklilue@ad.unc.edu>
2022-03-16 11:25:40 -0400

    yes

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -1,35 +1,40 @@
 """EX06: Dictionary Functions."""
 __author__ = "730477179."
 
-def invert(x:dict[str, str]) -> dict[str, str]:
+
+def invert(x: dict[str, str]) -> dict[str, str]:
     """Swaps Key and Values."""
-    first_string: str = ""
-    second_string: str = ""
-    regular: dict[str, str] = {first_string: second_string}
-    inverted: dict[str,str] = {second_string: first_string}
-    i: int = 0
-    while i < len(regular):
-        first_string = second_string
-        second_string = first_string
-        inverted[second_string] = first_string
-        i = i + 1
+    inverted: dict[str, str] = {}
+    for key in x:
+        inverted[x[key]] = key
     return inverted
 
 
-def favorite_colors(x: dict[str,str]) -> str:
-    favorite_colors_holder: str = ""
-    return favorite_colors_holder
-
 def count(x: list[str]) -> dict[str, int]:
+    """Counts occurences of strings."""
     result_dictionary: dict[str, int] = {}
     i: int = 0
     while i < len(x):
-        if x[i] in x == True:
-            result_dictionary[x[i]]+= 1 
-            i += 1
-        elif x[i] in x == False:
+        if x[i] in result_dictionary:
+            result_dictionary[x[i]] += 1 
+        else: 
             result_dictionary[x[i]] = 1
-            i += 1
+        i += 1
         
     return result_dictionary
 
+
+def favorite_color(x: dict[str, str]) -> str:
+    """Returns String of Favorite Color."""
+    favorite_colors_holder: list[str] = []
+    string_for_answer: str = ""
+    for key in x:
+        favorite_colors_holder.append(x[key])
+    counted_dictionary: dict[str, int] = count(favorite_colors_holder)
+    max: int = 0 
+    for key in counted_dictionary:
+        if counted_dictionary[key] > max:
+            max = counted_dictionary[key]
+            string_for_answer = key
+          
+    return string_for_answer
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -1,49 +1,58 @@
-"""Test for our dictionary functions"""
+"""Test for our dictionary functions."""
 __author__ = """730477179."""
 
-from dictionary import invert, count, favorite_colors
+from dictionary import invert, count, favorite_color
+
 
 def test_invert_one() -> None:
     """Testing Invert with two Key-Value Pairs."""
-    invert_one: dict[str, str] = {"hello":"kitty", "kit":"kat"}
-    assert invert(invert_one) == {"kitty":"hello", "kat": "kit"}
+    invert_one: dict[str, str] = {"hello": "kitty", "kit": "kat"}
+    assert invert(invert_one) == {"kitty": "hello", "kat": "kit"}
 
 
 def test_invert_two() -> None:
-    """Testing Invert with one Key- Value Pairs. """
-    invert_two: dict[str, str] = {"sun":"rise", "bed": "time", "baby":"girl"}
-    assert invert(invert_two) == {"rise":"sun", "time":"bed", "girl": "baby"}
+    """Testing Invert with one Key-Value Pairs."""
+    invert_two: dict[str, str] = {"sun": "rise", "bed": "time", "baby": "girl"}
+    assert invert(invert_two) == {"rise": "sun", "time": "bed", "girl": "baby"}
+
 
 def test_invert_three() -> None:
-    """Edge Case"""
-
+    """Edge Case."""
+    invert_three: dict[str, str] = {}
+    assert invert(invert_three) == {}
+    
 
 def test_count_one() -> None:
-    """ Edge case: Testing a Three- way tie"""
+    """Edge case Testing a Three- way tie."""
+    count_three: list[str] = []
+    count(count_three) == {}
 
 
 def test_count_two() -> None:
-    """ Use Case: """
+    """Use Case Two."""
     count_two: list[str] = ["blue", "blue", "red", "yellow"]
-    assert count(count_two) == {"blue": 2, "red": 1, "yellow": 1 }
+    assert count(count_two) == {"blue": 2, "red": 1, "yellow": 1}
 
 
 def test_count_three() -> None:
     """Another Use Case."""
     count_three: list[str] = ["blue", "blue", "blue", "blue"]
-    assert count(count_three) == {"blue" : 4}
+    assert count(count_three) == {"blue": 4}
 
 
 def test_favorite_colors_one() -> None:
     """Use Case 1."""
-    favorite_colors_one: dict[str,str] = {"Lily": "Blue", "Margaret":"Red", "Mary": "Blue"}
-    assert favorite_colors(favorite_colors_one) == "blue"
+    favorite_colors_one: dict[str, str] = {"Lily": "Blue", "Margaret": "Red", "Mary": "Blue"}
+    assert favorite_color(favorite_colors_one) == "Blue"
 
 
 def test_favorite_colors_two() -> None:
     """Use Case 2."""
-    favorite_colors_two: dict[str,str] = {"Lily" : "Purple", "Anna": "Green", "Helen": "Green"}
-    assert favorite_colors(favorite_colors_two) == "Green"
+    favorite_colors_two: dict[str, str] = {"Lily": "Purple", "Anna": "Green", "Helen": "Green"}
+    assert favorite_color(favorite_colors_two) == "Green"
 
-def test_favorirte_colors_three() -> None:
+
+def test_favorite_colors_three() -> None:
     """Edge Case."""
+    edge_case: dict[str, str] = {}
+    favorite_color(edge_case) == ""
